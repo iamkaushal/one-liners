1f41598fafa1121cbbb6d7dd305e40f4ba12c8ca
iamkaushal <kaushalsharma880@gmail.com>
2017-12-23 22:40:43 +0530

    added oneliner.py

---
+++ oneliner.py
@@ -0,0 +1,18 @@
+import json, io
+from random import randint
+
+def random_digits(oneliners_size):
+    # Return a joke index between first and last joke in data
+    return randint(0, oneliners_size-1)
+
+def getOneLiner():
+    # Read JSON file
+    with open('data.json') as data_file:
+        data_loaded = json.load(data_file)
+
+    oneliner = data_loaded['oneliners'][random_digits(len(data_loaded['oneliners']))]
+
+    print oneliner
+
+    ret_this = json.dumps({'oneliner' : oneliner})
+    return ret_this
